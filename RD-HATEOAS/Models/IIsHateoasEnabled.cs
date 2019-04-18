1b856f98addac18503d66473e771eebc130f3963
Maximilian Coutuer <Maximilian.Coutuer@realdolmen.com>
2019-04-18 17:00:48 +0200

    WIP

---
+++ RD-HATEOAS/Models/IIsHateoasEnabled.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RDHATEOAS.Models
+{
+    interface IIsHateoasEnabled
+    {
+
+    [NotMapped]
+    [JsonProperty(PropertyName = "_links")]
+    public HateoasLink[] Links { get; set; }
+    }
+}
